d942a6ba6572f479b49c478c552fb4fb87038e14
Patrick Elliott <patrick.d.elliott@gmail.com>
2017-08-19 12:23:24 +0200

    Added deploy command

--- site.hs
+++ site.hs
@@ -97,3 +97,8 @@ bibtexCompiler cslFileName bibFileName = do
     bib <- load $ fromFilePath bibFileName
     liftM writePandoc
         (getResourceBody >>= readPandocBiblio def csl bib)
+
+config :: Configuration
+config = defaultConfiguration
+    { deployCommand = "git stash && git checkout develop && stack exec site clean && stack exec site build && git fetch --all && git checkout -b master --track origin/master && cp -a _site/. . && git add -A && git commit -m \"Publish.\" && git push origin master:master && git checkout develop && git branch -D master && git stash pop"
+    }
