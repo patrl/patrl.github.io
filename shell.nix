cf23779fa860417089f44508c0941bb784d60706
Patrick Elliott <patrick.d.elliott@gmail.com>
2017-11-28 14:01:55 +0100

    Added deps

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@
 pkgs.haskell.lib.buildStackProject {
   name = "patrickdelliott.com";
   inherit ghc;
-  buildInputs = with pkgs; [ zlib gmp ];
+  buildInputs = with pkgs; [ zlib gmp git stack ];
   LANG = "en_US.UTF-8";
   TMPDIR = "/tmp";
 }
