a5ffd26d199fef92641269f4583a130cecb2fe2e
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-02-12 13:34:37 +0100

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/run/current-system/sw/bin/env nix-shell
-#!nix-shell -i bash -p git stack nix-shell 
+#!nix-shell -i bash -p git stack
 
 git stash && git checkout develop && stack exec site clean && stack exec site build && git fetch --all && git checkout -b master --track origin/master && cp -a _site/. . && git add -A && git commit -m \"Publish.\" && git push origin master:master && git checkout develop && git branch -D master && git stash pop
