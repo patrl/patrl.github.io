98fe93651255b150c1cf4e215bfc7249f13deb19
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-10-17 10:55:13 +0200

    refactor deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,18 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-git stash && git checkout develop && cabal run site clean && cabal run site build && git fetch --all && git checkout -b master --track origin/master && cp -a _site/. . && git add -A && git commit -m \"Publish.\" && git push origin master:master && git checkout develop && git branch -D master && git stash pop
+dirhash=$(ipfs add -r _site/ | grep _site$ | cut -d" " -f2)
+
+git stash
+git checkout develop
+cabal run site clean
+cabal run site build
+ipfs name publish $dirhash
+git fetch --all
+git checkout -b master --track origin/master
+cp -a _site/. .
+git add -A
+git commit -m \"Publish.\"
+git push origin master:master
+git checkout develop
+git branch -D master
+git stash pop
