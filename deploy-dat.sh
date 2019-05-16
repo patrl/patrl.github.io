9562007892a330ad173c7c380fffd483cf5d5956
Patrick Elliott <patrick.d.elliott@gmail.com>
2019-05-16 19:34:00 +0200

    Got rid of dat deploy

--- deploy-dat.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-git stash && git checkout develop && cabal run site clean && cabal run site build && find ~/dat/patrickdelliott.com/ -mindepth 1 -maxdepth 1 \! \( -name .dat -o -name dat.json \) -exec rm -rf '{}' \; && cp -a _site/. ~/dat/patrickdelliott.com/. && git stash pop && cd ~/dat/patrickdelliott.com && dat share --watch false
