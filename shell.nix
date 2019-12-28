a49c3298871d3c1524946f96165ac903da65de12
Patrick Elliott <patrick.d.elliott@gmail.com>
2019-12-28 11:45:12 -0500

    nix project boilerplate

---
+++ shell.nix
@@ -0,0 +1,32 @@
+let
+  # Look here for information about how to generate `nixpkgs-version.json`.
+  #  → https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs
+  pinnedVersion = builtins.fromJSON (builtins.readFile ./.nixpkgs-version.json);
+  pinnedPkgs = import (builtins.fetchGit {
+    inherit (pinnedVersion) url rev;
+
+    ref = "nixos-unstable";
+  }) {};
+in
+
+# This allows overriding pkgs by passing `--arg pkgs ...`
+{ pkgs ? pinnedPkgs }:
+
+with pkgs;
+
+mkShell {
+  buildInputs = [
+    pkgs.haskell.compiler.ghc865
+    pkgs.git
+    pkgs.zlib
+    pkgs.cabal-install
+    pkgs.pkgconfig
+    pkgs.which
+    pkgs.nodejs
+    pkgs.haskellPackages.brittany
+  ];
+
+  LD_LIBRARY_PATH="${pkgs.zlib}/lib";
+  SSL_CERT_FILE = "/etc/ssl/certs/ca-bundle.crt";
+  GIT_SSL_CAINFO = "/etc/ssl/certs/ca-certificates.crt";
+}
