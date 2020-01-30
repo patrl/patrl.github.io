be0f419c97adbc491abc9c3e0d09f5a693f7d803
Patrick Elliott <patrick.d.elliott@gmail.com>
2020-01-30 15:58:12 -0500

    simplified envrc (based on lorri)

--- shell.nix
+++ shell.nix
@@ -1,32 +1,53 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc865" }:
 let
-  # Look here for information about how to generate `nixpkgs-version.json`.
-  #  → https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs
-  pinnedVersion = builtins.fromJSON (builtins.readFile ./.nixpkgs-version.json);
-  pinnedPkgs = import (builtins.fetchGit {
-    inherit (pinnedVersion) url rev;
-    ref = "nixos-unstable";
-  }) { config = {allowBroken = true;}; };
+  inherit (nixpkgs) pkgs;
+  ghc = pkgs.haskell.packages.${compiler}.ghcWithPackages (ps: with ps; [
+          cabal-install 
+          brittany
+        ]);
 in
-
-# This allows overriding pkgs by passing `--arg pkgs ...`
-{ pkgs ? pinnedPkgs }:
-
-with pkgs;
-
-mkShell {
+pkgs.stdenv.mkDerivation {
+  name = "my-haskell-env-0";
   buildInputs = [
-    pkgs.haskell.compiler.ghc865
-    pkgs.git
-    pkgs.zlib
-    pkgs.cabal-install
-    pkgs.pkgconfig
-    pkgs.which
+    ghc
+    (import (builtins.fetchTarball "https://github.com/hercules-ci/ghcide-nix/tarball/master") {}).ghcide-ghc865
     pkgs.nodejs
-    pkgs.haskellPackages.brittany
+    pkgs.zlib
   ];
-
+  shellHook = "eval $(egrep ^export ${ghc}/bin/ghc)";
   LD_LIBRARY_PATH="${pkgs.zlib}/lib";
-  SSL_CERT_FILE = "/etc/ssl/certs/ca-bundle.crt";
-  GIT_SSL_CAINFO = "/etc/ssl/certs/ca-certificates.crt";
-  LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
 }
+
+
+# let
+#   # Look here for information about how to generate `nixpkgs-version.json`.
+#   #  → https://nixos.wiki/wiki/FAQ/Pinning_Nixpkgs
+#   pinnedVersion = builtins.fromJSON (builtins.readFile ./.nixpkgs-version.json);
+#   pinnedPkgs = import (builtins.fetchGit {
+#     inherit (pinnedVersion) url rev;
+#     ref = "nixos-unstable";
+#   }) { config = {allowBroken = true;}; };
+# in
+
+# # This allows overriding pkgs by passing `--arg pkgs ...`
+# { pkgs ? pinnedPkgs }:
+
+# with pkgs;
+
+# mkShell {
+#   buildInputs = [
+#     pkgs.haskell.compiler.ghc865
+#     pkgs.git
+#     pkgs.zlib
+#     pkgs.cabal-install
+#     pkgs.pkgconfig
+#     pkgs.which
+#     pkgs.nodejs
+#     pkgs.haskellPackages.brittany
+#   ];
+
+#   LD_LIBRARY_PATH="${pkgs.zlib}/lib";
+#   SSL_CERT_FILE = "/etc/ssl/certs/ca-bundle.crt";
+#   GIT_SSL_CAINFO = "/etc/ssl/certs/ca-certificates.crt";
+#   LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+# }
