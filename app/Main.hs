1e8d981a911c3f9d3ae46df830594c7beed147c6
Patrick Elliott <patrick.d.elliott@gmail.com>
2020-06-04 18:11:55 -0400

    Started outlining basic structure

---
+++ app/Main.hs
@@ -0,0 +1,20 @@
+module Main where
+
+import           Slick
+
+siteMeta :: SiteMeta
+siteMeta = SiteMeta { siteAuthor = "Patrick D, Elliott"
+                    , baseUrl = "https://patrickdelliott.com"
+                    , siteTitle = "Patrick D. Elliott"
+                    , twitterHandle = Just "patrickdelliott"
+                    , githubUser = Just "patrl" }
+
+data SiteMeta = SiteMeta { siteAuthor :: String
+                         , baseUrl :: String
+                         , siteTitle :: String
+                         , twitterHandle :: Maybe String
+                         , githubUser :: Maybe String }
+
+main :: IO ()
+main = do
+  print "hello haskell!"
