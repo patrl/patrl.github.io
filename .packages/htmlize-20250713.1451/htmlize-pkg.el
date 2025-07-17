da6236f48439d0beaf2deb063ca0df756f8a4c3f
Patrick Elliott <patrick.d.elliott@gmail.com>
2025-07-17 15:52:09 +0200

    "Publish."

---
+++ .packages/htmlize-20250713.1451/htmlize-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "htmlize" "20250713.1451"
+  "Convert buffer text and decorations to HTML."
+  '((emacs "26.1"))
+  :url "https://github.com/emacsorphanage/htmlize"
+  :commit "e390792b84aa61df29f899de118a8d93baf421b0"
+  :revdesc "e390792b84aa"
+  :keywords '("hypermedia" "extensions")
+  :authors '(("Hrvoje Niksic" . "hniksic@gmail.com"))
+  :maintainers '(("Hrvoje Niksic" . "hniksic@gmail.com")))
